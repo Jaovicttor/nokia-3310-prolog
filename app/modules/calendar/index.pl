996f3585c5091adf0555aa648da8dc8a097c4d50
Letícia Farias <100238247+LeticiaFarias21@users.noreply.github.com>
2023-06-12 02:49:05 -0300

    Update index.pl

--- app/modules/calendar/index.pl
+++ app/modules/calendar/index.pl
@@ -1,8 +1,8 @@
 :-use_module(library(csv)).
 
-/* Ler um arquivo csv e retorna uma lista de lista. */
-lerArquivoCsv(Lists):-
-    csv_read_file('/Users/lelefarias/Documents/nokia-3310-prolog/app/modules/calendar/data/dados.csv', Rows, []),
+/* Metodos para persistência. */
+readCsv(Lists):-
+    csv_read_file('../modules/calendar/data/dados.csv', Rows, []),
     rows_to_lists(Rows, Lists).
 
 rows_to_lists(Rows, Lists):- maplist(row_to_list, Rows, Lists).
@@ -11,41 +11,44 @@ row_to_list(Row, List):-
     Row =.. [row|List].
 
 listEvent :-
-    lerArquivoCsv(Resultado),
-    imprimirResultado(Resultado, Resultado).
+    readCsv(Resultado),
+    printResult(Resultado, Resultado).
 
-imprimirResultado([], _).
-imprimirResultado([Head|Tail], FirstList) :-
+printResult([], _).
+printResult([Head|Tail], FirstList) :-
     element_index(Head, FirstList, Index),
     write(Index), write(' - '), writeln(Head),
-    imprimirResultado(Tail, FirstList).
+    printResult(Tail, FirstList).
 
 element_index(Element, List, Index) :-
     nth1(Index, List, Element).
 
 addEventDB(Nome, Data, Comentario) :-
-    open('/Users/lelefarias/Documents/nokia-3310-prolog/app/modules/calendar/data/dados.csv', append, Fluxo),
+    open('../modules/calendar/data/dados.csv', append, Fluxo),
     nl(Fluxo),
     format(Fluxo, '~w  - ~w - ~w', [Nome, Data, Comentario]),
     close(Fluxo).
 
-limpaCsv:-
-    open('/Users/lelefarias/Documents/nokia-3310-prolog/app/modules/calendar/data/dados.csv', write, Fluxo),
+cleanCsv:-
+    open('../modules/calendar/data/dados.csv', write, Fluxo),
     write(Fluxo, ''),
     close(Fluxo).
 
 writeCsv(Line):-
-    open('/Users/lelefarias/Documents/nokia-3310-prolog/app/modules/calendar/data/dados.csv', append, Fluxo),
+    open('../modules/calendar/data/dados.csv', append, Fluxo),
     format(Fluxo, '~w', Line),
     close(Fluxo).
 
-menu :-
-    write('Menu:'), nl,
-    write('1. Adicionar Evento'), nl,
-    write('2. Remover Evento'), nl,
-    write('3. Listar Todos os Eventos'), nl,
-    write('4. Deletar Todos os Eventos'), nl,
-    write('5. Voltar'), nl,
+/* Metodos para escolha de menu */
+menuCalendar :-
+    writeln('-------------------NOKIA-------------------'),  
+    writeln('-------------------menu:-------------------'),
+    writeln('  1. Adicionar Evento'),
+    writeln('  2. Remover Evento'),
+    writeln('  3. Listar Todos os Eventos'),
+    writeln('  4. Deletar Todos os Eventos'),
+    writeln('  5. Voltar'),
+    writeln('-------------------------------------------'),
     read_option(Choice),
     process_option(Choice).
 
@@ -54,38 +57,44 @@ read_option(Choice) :-
     read(Choice).
 
 process_option(1) :-
-    write('Adicionar Evento'), nl, adicionarEvento. 
+    write('Adicionar Evento'), nl, addEvent. 
 
 process_option(2) :-
-    write('Remover Evento'), nl, removerEvento. 
+    write('Remover Evento'), nl, removeEvent. 
 
 process_option(3) :-
-    write('Listar Todos os Eventos'), nl, listarTodosEventos. 
+    write('Listar Todos os Eventos'), nl, listAll. 
 
 process_option(4) :-
-    write('Deletas Todos os Eventos'), nl, limpaCsv, menu. 
+    write('Deletas Todos os Eventos'), nl, cleanCsv, menuCalendar. 
 
 process_option(5) :-
      write('Voltar'), nl. 
 
 process_option(_) :-
-    write('Opção inválida!'), nl, menu.
+    write('Opção inválida!'), nl, menuCalendar.
 
-adicionarEvento :-
-    write('-----Adicionar Evento-----'), nl,
-    write('Nomes e Comentários com espaçamento devem ser postos entre aspas simples.'), nl,
+/* Metodos de funcinalidades do programa*/
+
+/* Adicionar */
+addEvent :-
+    writeln('--------------Adicionar Evento-------------'),
+    writeln('Nomes e Comentários com espaçamento devem'),
+    writeln('ser postos entre aspas simples.'),
     write('Nome do evento: '), nl,
     read(Nome),
-    write('Data do evento (DD/MM/AAAA): '), nl,
+    writeln('Data do evento (DD/MM/AAAA): '), 
     write('Dia: '), read(Dia), 
     write('Mês: '), read(Mes), 
     write('Ano: '), read(Ano), 
     write('Escreva um Comentário: '), nl,
     read(Comentario),
-    cadastrarEvento(Nome, Dia, Mes, Ano, Comentario),
-    write('Evento cadastrado com sucesso!'), nl, menu.
+    registerEvent(Nome, Dia, Mes, Ano, Comentario),
+    writeln('Evento cadastrado com sucesso!'), 
+    writeln('-------------------------------------------'),
+    menuCalendar.
 
-cadastrarEvento(Nome, Dia, Mes, Ano, Comentario):-
+registerEvent(Nome, Dia, Mes, Ano, Comentario):-
   Dia > 0 -> Dia =< 31,
   Mes > 0 -> Mes =< 12,
   Ano > 0 -> Ano =< 9999,
@@ -94,25 +103,27 @@ cadastrarEvento(Nome, Dia, Mes, Ano, Comentario):-
   addEventDB(Nome, Data, Comentario),
   nl, !.
 
-cadastrarEvento(_, _, _, _, _):-
+registerEvent(_, _, _, _, _):-
   write('Data inválida!'), nl, !.
 
-removerEvento :-
+/* Remover */
+removeEvent :-
+    writeln('---------------Remover Evento--------------'),
     listEvent, nl,
     write('Index do evento: '),
     read(Index),
-    lerArquivoCsv(ListaEventos),
+    readCsv(ListaEventos),
     remove_element_by_index(Index, ListaEventos, ListaEventosAtualizada),
-    limpaCsv,
-    reescreveEvento(ListaEventosAtualizada),
-    menu.
+    cleanCsv,
+    rewriteEvent(ListaEventosAtualizada),
+   writeln('--------------------------------------------'),
+    menuCalendar.
 
-reescreveEvento([]):-
+rewriteEvent([]):-
     write('Evento excluido com sucesso!').
-reescreveEvento([Head|Tail]):-
+rewriteEvent([Head|Tail]):-
     writeCsv(Head),
-    reescreveEvento(Tail).
-    
+    rewriteEvent(Tail).
 
 remove_element_by_index(Index, List, NewList):-
     newIndex(Index, NI),
@@ -123,18 +134,9 @@ remove_element_by_index(Index, List, NewList):-
 newIndex(Index, Result):- 
     Result is Index - 1.
 
-listarTodosEventos :-
-  nl, listEvent, nl, menu.
-
-clean_csv_file(File) :-
-    csv_read_file(File, Rows),
-    process_rows(Rows, CleanedRows),
-    csv_write_file(File, CleanedRows, []).
-
-process_rows([], []).
-process_rows([Row | Rows], [CleanedRow | CleanedRows]) :-
-    process_row(Row, CleanedRow),
-    process_rows(Rows, CleanedRows).
-
-process_row(Row, CleanedRow) :-
-    exclude(=(unwanted_value), Row, CleanedRow).
+/* Listar */
+listAll :-
+  writeln('----------Listar Todos os Eventos---------'),
+  nl, listEvent, nl,
+ writeln('-------------------------------------------'),
+  menuCalendar.
